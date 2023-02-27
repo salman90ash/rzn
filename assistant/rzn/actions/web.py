e078606184a9c5a638f7cd193c6c111b210bfc5b
Vbox ubuntu <ash-salman@yandex.ru>
2023-02-27 22:09:00 +0300

    fix v2

--- assistant/rzn/actions/web.py
+++ assistant/rzn/actions/web.py
@@ -16,9 +16,9 @@ def get_page(url: str, proxy: bool = False, background: bool = False):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("start-maximized")
     if background:
-        chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--window-size=1920,1080')
+        chrome_options.add_argument('headless')
+    chrome_options.add_argument('no-sandbox')
+    chrome_options.add_argument('window-size=1920,1080')
     chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
     chrome_options.add_experimental_option('useAutomationExtension', False)
     global PROXY_OPTIONS
