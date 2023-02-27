fd28bb1dd6b1d02db3df1e6a8d67a402ee93e1a5
Vbox ubuntu <ash-salman@yandex.ru>
2023-02-27 21:47:36 +0300

    fix get_page

--- assistant/rzn/actions/web.py
+++ assistant/rzn/actions/web.py
@@ -16,7 +16,7 @@ def get_page(url: str, proxy: bool = False, background: bool = False):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("start-maximized")
     if background:
-        chrome_options.add_argument('headless')
+        chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--window-size=1920,1080')
     chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
